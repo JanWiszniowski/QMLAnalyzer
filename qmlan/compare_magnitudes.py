2d40e386a756c0d850bf96a73e10255231b6737b
jwisz <jwisz@igf.edu.pl>
2026-07-09 10:12:47 +0200

    Step4

--- qmlan/compare_magnitudes.py
+++ qmlan/compare_magnitudes.py
@@ -27,14 +27,21 @@ def compare_magnitude(catalog, type1, type2, method_id1=None, method_id2=None):
         magnitudes2.append(magnitude2)
     return magnitudes1, magnitudes2
 
-def  ML2Mw(ML, method='Grunthal'):
-    ML2 = ML ** 2
+def  ML2Mw(ml, method='Grunthal'):
+    ml2 = ml ** 2
     if method == 'Grunthal':
-        Mw = 0.53 + 0.646 * ML + 0.0376 * ML2 # Grunthal 2009
+        mw = 0.53 + 0.646 * ml + 0.0376 * ml2 # Grunthal 2009
     else:
-        Mw = 1.2 + 0.28 * ML + 0.06 * ML2
-    return Mw
+        mw = 1.2 + 0.28 * ml + 0.06 * ml2
+    return mw
 
+def Mw2ML(mw, method='Grunthal'):
+    if method == 'Grunthal':
+        delta = np.sqrt(0.646 ** 2 - 4 * (0.53 - mw) * 0.0376)
+        ml = (delta - 0.646) / 0.0376 / 2.0
+    else:
+        ml = mw
+    return ml
 
 def func2 (x, a, b):
     return a + b * x
@@ -200,5 +207,174 @@ def main():
     plt.show(block=True)
 
 
+def main_ML():
+    parser = argparse.ArgumentParser(
+        prog='compare_magnitudes',
+        description='Program plot two type magnitudes relation')
+    parser.add_argument('-c', '--catalog', metavar='catalog_name.xml', help='Catalog')
+    parser.add_argument('-4', '--method2', metavar='', help='method of second magnitudes')
+    parser.add_argument('-i', '--input_format', default='QUAKEML', help='Input catalog format')
+    args = parser.parse_args()
+    print(f"Start reading {args.catalog}")
+    catalog = read_events(args.catalog, format=args.input_format)
+    print(f"Read {len(catalog.events)} events")
+    magnitudes1, magnitudes2 = compare_magnitude(catalog, 'Energy', 'Mw', method_id2=args.method2)
+    e = []
+    mw = []
+    for magnitude1, magnitude2 in zip(magnitudes1, magnitudes2):
+        if magnitude1 is not None and magnitude2 is not None:
+            e.append(magnitude1.mag)
+            mw.append(magnitude2.mag)
+    print(f"Found {len(e)} pairs")
+    fig1, ax1 = plt.subplots()
+    ax1.semilogx(e, mw, 'r.', markersize=2, label="Mw vs Energy")
+    ax1.set_xlabel("E [J]")
+    ax1.set_ylabel("Mw")
+    xxx = np.log10(np.array(e))
+    yyy = np.array(mw)
+    result = stats.linregress(xxx, yyy, 'two-sided')
+    xx = np.sort(np.array(e))
+    plt.semilogx(xx, result.intercept + result.slope * np.log10(xx), 'g--',
+                 label=r'$Mw={:.2}+{:.2} \cdot {}E [J]$'.format(result.intercept, result.slope, r"log_{10}"))
+    pars = fit2(yyy, xxx)
+    yy = np.sort(yyy)
+    ax1.semilogx(np.power(10.0, func2(yy, *pars)), yy, 'b--', linewidth=1,
+                 label=r"${}={:.2}+{:.2}\cdot Mw$".format("log_{10}E", pars[0], pars[1])
+                 )
+    ax1.legend()
+    ml = [Mw2ML(x) for x in mw]
+    fig2, ax2 = plt.subplots()
+    ax2.plot(ml, mw, 'bo', label="Mw vs ML")
+    xx = np.sort(np.array(ml))
+    yy = ML2Mw(xx)
+    ax2.plot(xx, yy, 'g--', linewidth=0.5, label="Gruntal (2009)")
+    #
+    fig3, ax3 = plt.subplots()
+    ax3.semilogx(e, ml, 'r.', markersize=2, label="Mw vs Energy")
+    ax3.set_xlabel("E [J]")
+    ax3.set_ylabel("M_L")
+    xxx = np.log10(np.array(e))
+    yyy = np.array(ml)
+    result = stats.linregress(xxx, yyy, 'two-sided')
+    xx = np.sort(np.array(e))
+    plt.semilogx(xx, result.intercept + result.slope * np.log10(xx), 'g--',
+                 label=r'$M_L={:.2}+{:.2} \cdot {}E [J]$'.format(result.intercept, result.slope, r"log_{10}"))
+    pars = fit2(yyy, xxx)
+    yy = np.sort(yyy)
+    # ax3.semilogx(np.power(10.0, func2(yy, *pars)), yy, 'b--', linewidth=1,
+    #              label=r"${}={:.2}+{:.2}\cdot Mw+{:.2}\cdot Mw^2$".format("log_{10}E", pars[0], pars[1])
+    #              )
+    ax3.semilogx(np.power(10.0, func2(yy, *pars)), yy, 'b--', linewidth=1,
+                 label=r"${}={:.2}+{:.2}\cdot M_L$".format("log_{10}E", pars[0], pars[1])
+                 )
+    ax3.legend()
+    #
+    plt.show(block=True)
+
+
+def testMLE():
+    catalog = read_events("Bogdanka.xml")
+    magnitudes1, magnitudes2 = compare_magnitude(catalog, 'Energy', 'ML', method_id2="smi:GIG/fromEnergy")
+    e = []
+    ml = []
+    for magnitude1, magnitude2 in zip(magnitudes1, magnitudes2):
+        if magnitude1 is not None and magnitude2 is not None:
+            e.append(magnitude1.mag)
+            ml.append(magnitude2.mag)
+    fig1, ax1 = plt.subplots()
+    ax1.semilogx(e, ml, 'r.', markersize=2, label="ML vs Energy")
+    xxx = np.log10(np.array(e))
+    yyy = np.array(ml)
+    pars = fit2(yyy, xxx)
+    yy = np.sort(yyy)
+    ax1.semilogx(np.power(10.0, func2(yy, *pars)), yy, 'b--', linewidth=1,
+                 label=r"${}={:.2}+{:.2}\cdot M_L$".format("log_{10}E", pars[0], pars[1])
+                 )
+    ax1.semilogx(np.power(10.0, 1.8 + 1.9 * yy), yy, 'g--', linewidth=1,
+                 label=r"$log_{10}E=1.8+1.9\cdot M_L$"
+                 )
+    ax1.legend()
+    plt.show(block=True)
+
+
+def test_Bogdanka():
+    parser = argparse.ArgumentParser(
+        prog='compare_magnitudes',
+        description='Program plot two type magnitudes relation')
+    parser.add_argument('-c', '--catalog', metavar='catalog_name.xml', help='Catalog')
+    parser.add_argument('-s', '--station_inventory', metavar='station_inventory.xml', help='Station inventory')
+    parser.add_argument('-o', '--output', metavar='output_catalog.xml', help='Output catalog')
+    parser.add_argument('-1', '--type1', metavar='Mw', help='Type of first magnitudes')
+    parser.add_argument('-2', '--type2', metavar='ML', help='Type of second magnitudes')
+    parser.add_argument('-3', '--method1', metavar='', help='Method of first magnitudes')
+    parser.add_argument('-4', '--method2', metavar='', help='method of second magnitudes')
+    parser.add_argument('-i', '--input_format', default='QUAKEML', help='Input catalog format')
+    parser.add_argument('-m', '--marker_size', default='3', help='Marker size')
+    parser.add_argument('-e', '--error_limit', default='0.4', help='Error limit')
+    parser.add_argument('-x', '--max_correct', default='3.2', help='Maximum corrected amplitudes')
+    args = parser.parse_args()
+    print(f"Start reading {args.catalog}")
+    catalog = read_events(args.catalog, format=args.input_format)
+    print(f"Read {len(catalog.events)} events")
+    magnitudes1, magnitudes2 = compare_magnitude(catalog, 'Energy', 'Mw', method_id2=args.method2)
+    e = []
+    y = []
+    for magnitude1, magnitude2 in zip(magnitudes1, magnitudes2):
+        if magnitude1 is not None and magnitude2 is not None:
+            e.append(magnitude1.mag)
+            y.append(magnitude2.mag)
+    x = [float(np.log10(ee) - 1.8) / 1.9 for ee in e]
+    print(f"Found {len(e)} pairs")
+    fig1, ax1 = plt.subplots()
+    min_mag, max_mag = min(x), max(x)
+    ax1.plot([min_mag, max_mag], [min_mag, max_mag], 'k:', linewidth=0.5, label=r"$Mw=ML$")
+    ax1.plot(x, y, 'r.', markersize=3, label="Estimated magnitudes")
+    xx = np.sort(np.array(x))
+    yy = ML2Mw(xx)
+    ax1.plot(xx, yy, 'b--', linewidth=0.5, label="Gruntal (2009)")
+    xxx = np.array(x)
+    yyy = np.array(y)
+    pars = fit3(xxx, yyy)
+    ax1.plot(xx, func3(xx, *pars), 'g--', linewidth=1,
+             label=r"$Mw={:.2}+{:.2}\cdot ML+{:.2}\cdot ML^2$".format(pars[0], pars[1], pars[2])
+             )
+    # bx = []
+    # by = []
+    # e = []
+    # if args.output:
+    #     catalog.events = e
+    #     catalog.write(args.output, format=args.input_format)
+    # ax1.plot(bx, by, 'b+', markersize=5, label="Outlier Mw values")
+    # ax1.legend()
+    # ax1.set_xlabel(args.type1 if args.method1 is None else f"{args.type1} ({args.method1})")
+    # ax1.set_ylabel(args.type2 if args.method2 is None else f"{args.type2} ({args.method2})")
+    # fig2, ax2 = plt.subplots()
+    # mag = [m.mag for m in magnitudes1 if m is not None]
+    # min_mag, max_mag = min(mag), max(mag)
+    # pre_bins = np.arange(np.floor(min_mag - 0.1), np.ceil(max_mag + 0.2), 0.1)
+    # counts, bins = np.histogram(mag, bins=pre_bins)
+    # ax2.semilogy(bins[:-1], counts, 'bo', markersize=3, label="No. events")
+    # ax2.semilogy(bins[:-1], np.cumsum(counts[::-1])[::-1], 'b-', label="Cumulated no. events")
+    # # ax2.hist([m.mag for m in magnitudes1 if m is not None], bins=100)
+    # ax2.set_xlabel(args.type1 if args.method1 is None else f"{args.type1} ({args.method1})")
+    # ax2.set_ylabel("No events in 0.1 bins")
+    # ax2.legend()
+    # fig3, ax3 = plt.subplots()
+    # mag = [m.mag for m in magnitudes2 if m is not None]
+    # min_mag, max_mag = min(mag), max(mag)
+    # pre_bins = np.arange(np.floor(min_mag - 0.1), np.ceil(max_mag + 0.2), 0.1)
+    # counts, bins = np.histogram(mag, bins=pre_bins)
+    # ax3.semilogy(bins[:-1], counts, 'bo', markersize=3, label="No. events")
+    # ax3.semilogy(bins[:-1], np.cumsum(counts[::-1])[::-1], 'b-', label="Cumulated no. events")
+    # # ax3.hist([m.mag for m in magnitudes2 if m is not None], bins=100)
+    # ax3.set_xlabel(args.type2 if args.method2 is None else f"{args.type2} ({args.method2})")
+    # ax3.set_ylabel("No. events in 0.1 bins")
+    # ax3.legend()
+    plt.show(block=True)
+
+
+
 if __name__ == '__main__':
-    main()
+    # main_ML()
+    # testMLE()
+    test_Bogdanka()
